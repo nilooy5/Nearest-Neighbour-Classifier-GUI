a4258ec28a5b83016f9ac4fafccd6cf0a10f0076
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-09-08 19:42:25 +0600

    unknown tuple changed to random tuple

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import io_data_module as io
 from renderer import render_points
 from nearest_neighbor import find_nearest_neighbor
+import random
 
 dataSet = io.read_multi_dim_data("other_datasets/ellipse1.txt")
 
-unknown_tuple = (5.0, 7.0)
+unknown_tuple = (random.uniform(-6.0, 6.0), random.uniform(-6.0, 6.0))
 
 nearest_tuple = find_nearest_neighbor(unknown_tuple, dataSet)
 
