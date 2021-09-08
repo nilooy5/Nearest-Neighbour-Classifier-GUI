3b4b651b51b69564510867fde64bf91250bc6040
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-09-08 20:02:10 +0600

    line not connecting points bug resolved

--- renderer.py
+++ renderer.py
@@ -5,7 +5,7 @@ tk_window = Tk()
 
 def render_points(dataSet, unknown_tuple, nearest_tuple):
     c = Canvas(tk_window, bg="white", height=700, width=1000)
-    radius = 4
+    radius = 3.5
     scale = 50
     offset = 300
 
@@ -15,16 +15,19 @@ def render_points(dataSet, unknown_tuple, nearest_tuple):
                       float(item[0]) * scale + radius + offset,
                       float(item[1]) * scale + radius + offset,
                       outline="black", fill="red")
-    c.create_line(float(unknown_tuple[0]) * scale - radius + offset,
-                  float(unknown_tuple[1]) * scale - radius + offset,
-                  float(nearest_tuple[0]) * scale - radius + offset,
-                  float(nearest_tuple[1]) * scale - radius + offset,
+
+    c.create_line(float(unknown_tuple[0]) * scale + offset,
+                  float(unknown_tuple[1]) * scale + offset,
+                  float(nearest_tuple[0]) * scale + offset,
+                  float(nearest_tuple[1]) * scale + offset,
                   fill="blue")
+
     c.create_oval(float(unknown_tuple[0]) * scale - radius + offset,
                   float(unknown_tuple[1]) * scale - radius + offset,
                   float(unknown_tuple[0]) * scale + radius + offset,
                   float(unknown_tuple[1]) * scale + radius + offset,
                   outline="black", fill="white")
+
     c.create_oval(float(nearest_tuple[0]) * scale - radius + offset,
                   float(nearest_tuple[1]) * scale - radius + offset,
                   float(nearest_tuple[0]) * scale + radius + offset,
