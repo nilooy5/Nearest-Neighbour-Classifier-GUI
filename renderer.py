1a39343e9dff959dbc972f382571843f7cdaaf5c
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-09-08 19:44:28 +0600

    function whitespace added for warning in render_points

--- renderer.py
+++ renderer.py
@@ -1,6 +1,8 @@
 from tkinter import *
 
 tk_window = Tk()
+
+
 def render_points(dataSet, unknown_tuple, nearest_tuple):
     c = Canvas(tk_window, bg="white", height=700, width=1000)
     radius = 1.5
@@ -19,14 +21,14 @@ def render_points(dataSet, unknown_tuple, nearest_tuple):
                   float(nearest_tuple[1]) * scale - radius + offset,
                   fill="blue")
     c.create_oval(float(unknown_tuple[0]) * scale - radius + offset,
-                      float(unknown_tuple[1]) * scale - radius + offset,
-                      float(unknown_tuple[0]) * scale + radius + offset,
-                      float(unknown_tuple[1]) * scale + radius + offset,
-                      outline="blue", fill="white")
+                  float(unknown_tuple[1]) * scale - radius + offset,
+                  float(unknown_tuple[0]) * scale + radius + offset,
+                  float(unknown_tuple[1]) * scale + radius + offset,
+                  outline="blue", fill="white")
     c.create_oval(float(nearest_tuple[0]) * scale - radius + offset,
-                      float(nearest_tuple[1]) * scale - radius + offset,
-                      float(nearest_tuple[0]) * scale + radius + offset,
-                      float(nearest_tuple[1]) * scale + radius + offset,
-                      outline="blue", fill="blue")
+                  float(nearest_tuple[1]) * scale - radius + offset,
+                  float(nearest_tuple[0]) * scale + radius + offset,
+                  float(nearest_tuple[1]) * scale + radius + offset,
+                  outline="blue", fill="blue")
     c.pack()
     tk_window.mainloop()
