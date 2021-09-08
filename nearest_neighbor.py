7dd83c79e54f39e806390a6ba2b66e986b21d2d8
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-09-09 00:08:42 +0600

    formatting changed

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -5,18 +5,21 @@ def find_nearest_neighbor(unknown_tuple, dataset_red, dataset_blue):
     dataset_color = "white"
     nearest_tuple = (math.inf, math.inf)
     smallest_distance = math.inf
+
     for item in dataset_red:
         distance = find_distance(unknown_tuple, item)
         if distance < smallest_distance:
             nearest_tuple = item
             smallest_distance = distance
             dataset_color = "red"
+
     for item in dataset_blue:
         distance = find_distance(unknown_tuple, item)
         if distance < smallest_distance:
             nearest_tuple = item
             smallest_distance = distance
             dataset_color = "blue"
+
     return [nearest_tuple, dataset_color]
 
 
