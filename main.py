9a062a40c9eb910dc455622f538873d548790d58
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-09-11 01:33:32 +0600

    file name selected through coding

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import io_data_module as io
 from renderer import render_graph
 from nearest_neighbor import find_nearest_neighbor
 
-dataSet_red = io.read_multi_dim_data("datasets/nearest-neighbor/red_4d.txt")
-dataSet_blue = io.read_multi_dim_data("datasets/nearest-neighbor/blue_4d.txt")
-dataset_unknown = io.read_multi_dim_data("datasets/nearest-neighbor/unknown_4d.txt")
+dimension = 2
+
+dataSet_red = io.read_multi_dim_data("datasets/nearest-neighbor/red_" + str(dimension) + "d.txt")
+dataSet_blue = io.read_multi_dim_data("datasets/nearest-neighbor/blue_" + str(dimension) + "d.txt")
+dataset_unknown = io.read_multi_dim_data("datasets/nearest-neighbor/unknown_" + str(dimension) + "d.txt")
 
 nearest_dataset = find_nearest_neighbor(dataset_unknown, dataSet_red, dataSet_blue)
 
