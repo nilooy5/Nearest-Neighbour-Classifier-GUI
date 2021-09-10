d9fa5c9ed6174dd7969f14401f1f72921e906c09
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-09-11 00:55:22 +0600

    unnecessary variables deleted

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import io_data_module as io
-import random
 from renderer import render_graph
 from nearest_neighbor import find_nearest_neighbor
 
-dataSet_red = io.read_multi_dim_data("datasets/nearest-neighbor/red_2d.txt")
-dataSet_blue = io.read_multi_dim_data("datasets/nearest-neighbor/blue_2d.txt")
-dataset_unknown = io.read_multi_dim_data("datasets/nearest-neighbor/unknown_2d.txt")
-
-unknown_tuple = (random.uniform(-6.0, 6.0), random.uniform(-6.0, 6.0))
+dataSet_red = io.read_multi_dim_data("datasets/nearest-neighbor/red_4d.txt")
+dataSet_blue = io.read_multi_dim_data("datasets/nearest-neighbor/blue_4d.txt")
+dataset_unknown = io.read_multi_dim_data("datasets/nearest-neighbor/unknown_4d.txt")
 
 nearest_dataset = find_nearest_neighbor(dataset_unknown, dataSet_red, dataSet_blue)
 
