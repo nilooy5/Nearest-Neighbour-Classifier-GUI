e8bbaed37e9c4667e863f097ce229e30811788b9
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-09-13 08:09:33 +0600

    shortest center for each point determined :tada: 😳

---
+++ k_means_clustering.py
@@ -0,0 +1,17 @@
+from nearest_neighbor import find_distance
+
+
+def generate_random_centroids(num_of_centroids):
+    print("okay")
+
+
+def find_closest_cluster_center(dataset, centroids):
+    for point in dataset:
+        distance = float("inf")
+        closest_center = float("inf")
+        for center in centroids:
+            local_distance = find_distance(point, center)
+            if local_distance < distance:
+                distance = local_distance
+                closest_center = center
+        print("point:",point, "center:",closest_center, "distance:", distance)
--- question_2.py
+++ question_2.py
@@ -1,9 +1,11 @@
 import io_data_module as io
 import renderer as render
+from k_means_clustering import find_closest_cluster_center
 
+dataset = io.read_multi_dim_data("datasets/K-means/data_2c_2d.txt")
 
-dataset_2c_2d = io.read_multi_dim_data("datasets/K-means/data_4c_4d.txt")
+find_closest_cluster_center(dataset,[(5.0, 6.0), (7.0, -1.0)])
 
-render.render_points(dataset_2c_2d, "blue")
+render.render_points(dataset, "blue")
 
 render.run_tk_window()
