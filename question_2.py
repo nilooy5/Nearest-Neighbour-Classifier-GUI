e55a6f93ddb46832e15945bdb846f359125c966e
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-09-17 16:48:17 +0600

    driver function changed

--- question_2.py
+++ question_2.py
@@ -2,14 +2,15 @@ import io_data_module as io
 import renderer as render
 from k_means_clustering import find_cluster_for_random_center
 
-dataset = io.read_multi_dim_data("datasets/K-means/data_2c_2d.txt")
+dataset = io.read_multi_dim_data("datasets/K-means/data_4c_2d.txt")
 
-final_clusters = find_cluster_for_random_center(dataset, [(5.0, 6.0), (7.0, -1.0)])[0]
+final_clusters = find_cluster_for_random_center(dataset, [(0.02, 0.82), (-0.004, 4.3), (3.8955, 2.392), (0.1, 2.1)])[0]
 
 for cluster in final_clusters:
     for item in final_clusters[cluster]:
         render.draw_line(item, cluster)
     render.render_points(final_clusters[cluster], "blue")
     render.render_points([cluster], "red")
+    render.draw_label(cluster, "")
 
 render.run_tk_window()
