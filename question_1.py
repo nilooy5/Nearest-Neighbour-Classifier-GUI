19874d1b5cc345c5d81d842d7564e1bc0b493f55
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-09-18 09:37:06 +0600

    driver file put in an infinite loop in question_1.py

--- question_1.py
+++ question_1.py
@@ -2,16 +2,22 @@ import io_data_module as io
 from renderer import render_graph
 from nearest_neighbor import generate_nearest_neighborhood_dataset, generate_neighborhood_dataset
 
-dimension = input("enter dimension of data (2/4/8): ")
+while True:
+    try:
+        dimension = int(input("enter dimension of data (2/4/8): "))
+        if dimension == 2 or dimension == 4 or dimension == 8:
+            dataSet_red = io.read_multi_dim_data("datasets/nearest-neighbor/red_" + str(dimension) + "d.txt")
+            dataSet_blue = io.read_multi_dim_data("datasets/nearest-neighbor/blue_" + str(dimension) + "d.txt")
+            dataset_unknown = io.read_multi_dim_data("datasets/nearest-neighbor/unknown_" + str(dimension) + "d.txt")
 
-dataSet_red = io.read_multi_dim_data("datasets/nearest-neighbor/red_" + str(dimension) + "d.txt")
-dataSet_blue = io.read_multi_dim_data("datasets/nearest-neighbor/blue_" + str(dimension) + "d.txt")
-dataset_unknown = io.read_multi_dim_data("datasets/nearest-neighbor/unknown_" + str(dimension) + "d.txt")
+            nearest_dataset = generate_nearest_neighborhood_dataset(dataset_unknown, dataSet_red, dataSet_blue)
 
-nearest_dataset = generate_nearest_neighborhood_dataset(dataset_unknown, dataSet_red, dataSet_blue)
+            neighborhood_dataset = generate_neighborhood_dataset(dataset_unknown, nearest_dataset)
 
-neighborhood_dataset = generate_neighborhood_dataset(dataset_unknown, nearest_dataset)
+            io.write_to_file(neighborhood_dataset, dimension)
 
-io.write_to_file(neighborhood_dataset, dimension)
-
-# render_graph(dataSet_red, dataSet_blue, dataset_unknown, nearest_dataset)
+            # render_graph(dataSet_red, dataSet_blue, dataset_unknown, nearest_dataset)
+        else:
+            raise ValueError
+    except ValueError:
+        print("please enter a valid number")
