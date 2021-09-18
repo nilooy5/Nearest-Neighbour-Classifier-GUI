a2211ef797db7146a53612d3d5ab6e439b01144e
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-09-18 07:38:16 +0600

    initial points moved to separate variable

--- question_2.py
+++ question_2.py
@@ -4,7 +4,9 @@ from k_means_clustering import find_cluster_for_random_center
 
 dataset = io.read_multi_dim_data("datasets/K-means/data_4c_2d.txt")
 
-final_clusters = find_cluster_for_random_center(dataset, [(0.02, 0.82), (-0.004, 4.3), (3.8955, 2.392), (0.1, 2.1)])[0]
+initial_points = [(0.02, 0.82), (-0.004, 4.3), (3.8955, 2.392), (0.1, 2.1)]
+
+final_clusters = find_cluster_for_random_center(dataset, initial_points)[0]
 
 render.render_clusters(final_clusters)
 
