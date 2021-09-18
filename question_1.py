bf52635468c3941e544e149e219d7f635fa1724c
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-09-18 07:36:08 +0600

    prompt added & rendering disabled by default

--- question_1.py
+++ question_1.py
@@ -2,7 +2,7 @@ import io_data_module as io
 from renderer import render_graph
 from nearest_neighbor import generate_nearest_neighborhood_dataset, generate_neighborhood_dataset
 
-dimension = 2
+dimension = input("enter dimension of data (2/4/8): ")
 
 dataSet_red = io.read_multi_dim_data("datasets/nearest-neighbor/red_" + str(dimension) + "d.txt")
 dataSet_blue = io.read_multi_dim_data("datasets/nearest-neighbor/blue_" + str(dimension) + "d.txt")
@@ -14,4 +14,4 @@ neighborhood_dataset = generate_neighborhood_dataset(dataset_unknown, nearest_da
 
 io.write_to_file(neighborhood_dataset, dimension)
 
-render_graph(dataSet_red, dataSet_blue, dataset_unknown, nearest_dataset)
+# render_graph(dataSet_red, dataSet_blue, dataset_unknown, nearest_dataset)
