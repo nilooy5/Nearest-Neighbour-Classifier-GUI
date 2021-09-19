a1323faa0643a54c738892e0b5441c1f928b53bc
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-09-19 07:34:44 +0600

    additional comments added

--- question_2.py
+++ question_2.py
@@ -2,8 +2,11 @@ import io_data_module as io
 import renderer as render
 from k_means_clustering import find_cluster_for_random_center
 
+
+# change the file name to test other files like data_2c_2d.txt data_2c_4d.txt data_4c_4d.txt
 dataset = io.read_multi_dim_data("datasets/K-means/data_4c_2d.txt")
 
+# change initial points according to the file name
 # points for 4C 2d
 initial_points = [(0.02, 0.82), (-0.004, 4.3), (3.8955, 2.392), (0.1, 2.1)]
 
