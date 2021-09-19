c50af07d55ed15d5c958615b731750ca26475406
Fazal Mahmud Niloy <niloooy@gmail.com>
2021-09-19 07:16:04 +0600

    while loop removed from question_1.py

--- question_1.py
+++ question_1.py
@@ -2,22 +2,22 @@ import io_data_module as io
 from renderer import render_graph
 from nearest_neighbor import generate_nearest_neighborhood_dataset, generate_neighborhood_dataset
 
-while True:
-    try:
-        dimension = int(input("enter dimension of data (2/4/8): "))
-        if dimension == 2 or dimension == 4 or dimension == 8:
-            dataSet_red = io.read_multi_dim_data("datasets/nearest-neighbor/red_" + str(dimension) + "d.txt")
-            dataSet_blue = io.read_multi_dim_data("datasets/nearest-neighbor/blue_" + str(dimension) + "d.txt")
-            dataset_unknown = io.read_multi_dim_data("datasets/nearest-neighbor/unknown_" + str(dimension) + "d.txt")
 
-            nearest_dataset = generate_nearest_neighborhood_dataset(dataset_unknown, dataSet_red, dataSet_blue)
+try:
+    dimension = int(input("enter dimension of data (2/4/8): "))
+    if dimension == 2 or dimension == 4 or dimension == 8:
+        dataSet_red = io.read_multi_dim_data("datasets/nearest-neighbor/red_" + str(dimension) + "d.txt")
+        dataSet_blue = io.read_multi_dim_data("datasets/nearest-neighbor/blue_" + str(dimension) + "d.txt")
+        dataset_unknown = io.read_multi_dim_data("datasets/nearest-neighbor/unknown_" + str(dimension) + "d.txt")
 
-            neighborhood_dataset = generate_neighborhood_dataset(dataset_unknown, nearest_dataset)
+        nearest_dataset = generate_nearest_neighborhood_dataset(dataset_unknown, dataSet_red, dataSet_blue)
 
-            io.write_to_file(neighborhood_dataset, dimension)
+        neighborhood_dataset = generate_neighborhood_dataset(dataset_unknown, nearest_dataset)
 
-            # render_graph(dataSet_red, dataSet_blue, dataset_unknown, nearest_dataset)
-        else:
-            raise ValueError
-    except ValueError:
-        print("please enter a valid number")
+        io.write_to_file(neighborhood_dataset, dimension)
+
+        # render_graph(dataSet_red, dataSet_blue, dataset_unknown, nearest_dataset)
+    else:
+        raise ValueError
+except ValueError:
+    print("please enter a valid number")
